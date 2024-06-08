87a3053066338305e7fc6b5f1fd8f3d7458a4a68
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-08 20:56:47 +0200

    added root index html page

--- postms/urls.py
+++ postms/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from jobposts.views import DefaultView
 
 urlpatterns = [
+    path('', DefaultView.as_view(), name="index"),
     path('admin/', admin.site.urls),
+    
 ]
