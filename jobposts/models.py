135079d577de6b5ba3911b04663655f915f92433
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-08 20:33:47 +0200

    added authors app

--- jobposts/models.py
+++ jobposts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class JobPost(models.Model):
+    author = models.ForeignKey(
+        "post author",
+        on_delete=models.CASCADE,
+        nul=False,
+        blank=False,
+    )
+    title = models.CharField(
+        "post title",
+        max_length=255,
+        nul=False,
+        blank=False,
+    )
