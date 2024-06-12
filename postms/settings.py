8da04e9ea91a6ae0019d020d9f1597f5e7c487b0
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-12 19:42:29 +0200

    updated changes to setting conf

--- postms/settings.py
+++ postms/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', 
     
     # DEPENDENCIES
     "widget_tweaks",
