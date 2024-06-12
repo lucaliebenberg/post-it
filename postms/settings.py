8f4b4a5335c92591f86d3477957777799ccfa7d0
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-12 19:49:34 +0200

    added auth redirects to settings

--- postms/settings.py
+++ postms/settings.py
@@ -13,7 +13,6 @@ except Exception:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -138,3 +137,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGIN 
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "index"
