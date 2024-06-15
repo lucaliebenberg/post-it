94cd6798a5f260a2d02696070ef7bde7d7ba5e32
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-15 10:37:13 +0200

    minor update to core API

--- postms/urls.py
+++ postms/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # AUTH
-    path('/register', CustomRegisterView.as_view(), name="register"),
+    path('register', CustomRegisterView.as_view(), name="register"),
     path('login', CustomLoginView.as_view(), name="login"),
     path('logout', CustomLogoutView.as_view(), name="logout"),
 
