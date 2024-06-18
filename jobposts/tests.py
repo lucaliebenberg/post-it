7f370dbc4774861008ec5b5f9068c511974cc2d6
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-18 21:42:19 +0200

    first tests on jobpost and DB

--- jobposts/tests.py
+++ jobposts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from jobposts.models import JobPost
 
-# Create your tests here.
+class JobPostTestCase(TestCase):
+    def setup(self):
+        JobPost.objects.create(creator='182', contact_num="09872612431",
+            title="Marketing Guru",
+            description="This is a hello world test",
+        )
+        
+    def test_animal_creation(self):
+        post = JobPost.objects.get(title='Marketing Guru')
+        self.assertEqual(post.title, "Marketing Guru")
