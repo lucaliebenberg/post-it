59db8a222275bb9ff427715b29e5d5eca633e533
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-07-04 22:15:11 +0200

    cleanup

--- postms/templatetags/permission_filter.py
+++ postms/templatetags/permission_filter.py
@@ -6,19 +6,5 @@ register = template.Library()
 
 @register.filter
 def is_post_author(user, post):
-    # post = JobPost.objects.get(pk=self.kwargs.get("pk"))
-    # post_creator = User.objects.get(email=post.creator)
-    # print("Post creator >>> ", post_creator)
-    # user = self.request.user
-    # print("Current user >>> ", user)
-    # if self.request.user is post_creator:
-    #     print(
-    #     """
-    #     This is the owner of the job post
-    #     """
-    #     )
-    #     return True
-    # else: 
-    #     raise Exception("User is not post author!!!")
     return user == post.creator
 
