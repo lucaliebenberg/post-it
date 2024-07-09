cff42ebddae1076b9adfa684f8747b742d9b67ca
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-07-09 21:42:14 +0200

    added config for nginx docker-compose, need to resolve python base version for compose-up

--- postms/settings.py
+++ postms/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# TODO: commnt out before prod deployment
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
@@ -147,7 +148,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
