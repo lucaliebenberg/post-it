8040ca712bda74bd6bb3393aefb98d13c4773c84
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-07-29 18:38:35 +0200

    minor changes to .sh script + Dockerfile migrate atargets

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,10 @@ RUN pip install --upgrade pip
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 
-COPY ./postms ./accounts ./jobposts ./authors  /app/
+# COPY ./postms ./accounts ./jobposts ./authors  /app/
+COPY ./postms  /app/
 
 WORKDIR /app
 
 COPY ./entrypoint.sh /
 ENTRYPOINT ["sh", "/entrypoint.sh"]
-
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python manage.py makemigrations postms accounts authors jobposts
-python manage.py migrate
-python manage.py collectstatic
+# python manage.py makemigrations postms accounts authors jobposts
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 
 gunicorn postms.wsgi:application --bind 0.0.0.0:8000
