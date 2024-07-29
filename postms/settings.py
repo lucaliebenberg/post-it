602339496f7796fa4f645665a4becc9de505b291
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-07-29 19:21:00 +0200

    added debug variable to .env file for security

--- postms/settings.py
+++ postms/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # TODO: commnt out before prod deployment
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True")
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
