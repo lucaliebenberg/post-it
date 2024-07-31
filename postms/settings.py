39fd96a1e8c19a61d499f318b1fbdd3731d899e3
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-07-31 23:20:18 +0200

    bugfix: think nginx bug is resovled and will correctly serve django app

--- postms/settings.py
+++ postms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "True")
 
 # ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
-ALLOWED_HOSTS = ['192.168.65.1', '*']
+ALLOWED_HOSTS = ['192.168.65.1', 'localhost', '127.0.0.1', 'django_gunicorn']
 
 AUTH_USER_MODEL = os.environ.get("AUTH_USER_MODEL")
 
