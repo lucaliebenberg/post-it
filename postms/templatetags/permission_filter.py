9c4d7cff2f7f731041e5f8e26ffa9a809b8cb5e1
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-08-25 12:58:31 +0200

    feat: added post creation perm_filter

--- postms/templatetags/permission_filter.py
+++ postms/templatetags/permission_filter.py
@@ -8,3 +8,10 @@ register = template.Library()
 def is_post_author(user, post):
     return user == post.creator
 
+@register.filter
+def check_user_total_posts(user, post):
+    posts_qs= JobPost.objects.all().filter(creator=user)
+    posts = list(posts_qs)
+    if len(posts) >= 1:
+        return False
+    return True
